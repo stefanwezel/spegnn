12cf25cc6690564849b652015ddb2004ee071fb3
Stefan <stefan.wezel@student.uni-tuebingen.de>
2021-12-21 18:37:48 +0100

    include egnn

--- sandbox.py
+++ sandbox.py
@@ -17,31 +17,30 @@ from scipy.spatial import distance
 from tqdm import tqdm
 from matplotlib.patches import Circle
 from PIL import Image
-# from egnn_clean import E_GCL, EGNN, get_edges_batch
 
+from models import EGNN, get_edges_batch
 
 
 
 def collate_edges(batch):
-    """ TODO """
+    """ TODO docstring"""
     batch_imgs = [b['img_rgb'] for b in batch]
+    batch_edges = [b['edges'] for b in batch]
+    batch_edges_rows = [b['edges'][0] for b in batch]
+    batch_edges_columns = [b['edges'][1] for b in batch]
     batch_locations = [b['locations'] for b in batch]
     batch_orientations = [b['orientations'] for b in batch]
     batch_targets = [b['target'] for b in batch]
     batch_segments = [b['segments'] for b in batch]
+
+    batch_edges_rows = torch.nn.utils.rnn.pad_sequence(batch_edges_rows, batch_first=True)
+    batch_edges_columns = torch.nn.utils.rnn.pad_sequence(batch_edges_columns, batch_first=True)
     batch_locations = torch.nn.utils.rnn.pad_sequence(batch_locations,batch_first=True)
     batch_orientations = torch.nn.utils.rnn.pad_sequence(batch_orientations,batch_first=True)
-    # print(batch_orientations.size())
-    # n_nodes = batch_locations.shape[1]
-    # batch_size = batch_locations.shape[0]
-    # edges, edge_attr = get_edges_batch(n_nodes, batch_size)
-    # batch_orientations = batch_orientations.view(-1,2) / 28
-    # batch_locations = batch_locations.view(batch_orientations.shape[0],-1)
-    # batch_targets = torch.LongTensor(batch_targets)
-    
-    # return [batch_locations,batch_orientations,batch_targets,edges,n_nodes,batch_size]
+
     return dict(
                 img=batch_imgs,
+                edges=[batch_edges_rows, batch_edges_columns],
                 locations=batch_locations,
                 orientations=batch_orientations,
                 target=batch_targets,
@@ -123,7 +122,6 @@ class SuperpixelDataset(torch.utils.data.Dataset):
         img_rgb[:,:,1] = img
         img_rgb[:,:,0] = img
 
-
         img = np.float32(np.asarray(img[0,:,:]))/255
         labels = slic(img_rgb, n_segments = 15, sigma = 0.1, start_label=0)
         p = regionprops(labels+1,intensity_image=img)
@@ -135,9 +133,15 @@ class SuperpixelDataset(torch.utils.data.Dataset):
         edge_locations = []
         edge_orientations = []
 
+        rows, columns = [], []
+
         for node in g.nodes:
             neighbors = get_neighbors(node, g.edges)
             highest_contrast_neighbor = get_highest_contrast_neighbor(g, node, neighbors)
+
+            rows.append(node)
+            columns.append(highest_contrast_neighbor)
+
             edge_center = (np.array(p[node].centroid) + np.array(p[highest_contrast_neighbor].centroid))/2
 
             edge_orientation = np.rad2deg(
@@ -147,26 +151,26 @@ class SuperpixelDataset(torch.utils.data.Dataset):
                     ))
             
             edge_locations.append(torch.Tensor(np.array(edge_center)))
-            # edge_locations.append(torch.Tensor(np.array(p[node].centroid)))
             edge_orientations.append(edge_orientation)
 
             color = p[node]['mean_intensity']
-            orientation = p[node].orientation#['orientation']
+            orientation = p[node].orientation
             invariants = p[node]['moments_hu']
             center = torch.Tensor(p[node]['centroid']).unsqueeze(0)
             feat = torch.cat([torch.Tensor([color]),torch.Tensor(invariants)]).unsqueeze(0)
             feats.append(feat)
             coords.append(center)
 
-        edge_locations = torch.stack(edge_locations, dim=0)#.unsqueeze(0)
-        edge_orientations = torch.Tensor(np.array(edge_orientations))#.unsqueeze(0)
+        edges = torch.stack((torch.LongTensor(rows), torch.LongTensor(columns)))
+        edge_locations = torch.stack(edge_locations, dim=0)
+        edge_orientations = torch.Tensor(np.array(edge_orientations))
+
         feats = torch.cat(feats,dim=0)
         coords = torch.cat(coords,dim=0)
 
-
-        # print(coords)
         return dict(
             img_rgb=img_rgb,
+            edges=edges,
             locations=edge_locations,
             orientations=edge_orientations,
             target=target,
@@ -196,15 +200,43 @@ trainloader = torch.utils.data.DataLoader(mnist_train, batch_size=10, shuffle=Tr
 sample = next(iter(trainloader))
 
 
-print(sample.keys())
 
-fig, ax = plt.subplots()
-ax.imshow(mark_boundaries(sample['img'][0], sample['segments'][0]))
-for i in range(sample['locations'][0].size(0)):
-    loc = sample['locations'][0][i]
-    orientation = sample['orientations'][0][i]
-    circ = Circle(loc, 0.3, color='red')
-    slope = np.tan(orientation.numpy())
-    plot_line(ax, loc, slope)
-    ax.add_patch(circ)
-plt.show()
+egnn = EGNN(in_node_nf=1, hidden_nf=32, out_node_nf=1, in_edge_nf=1)
+# # Dummy parameters
+batch_size = 10
+n_nodes = sample['edges'][0][1].size(0)
+n_feat = 1
+x_dim = 2
+
+# # Dummy variables h, x and fully connected edges
+# h := node embeddings
+h = torch.ones(batch_size *  n_nodes, n_feat)
+# x := coordinate embeddings
+x = torch.ones(batch_size * n_nodes, x_dim)
+
+
+# fake edges, edge_attributes
+edges, edge_attr = get_edges_batch(n_nodes, batch_size)
+edges = [sample['edges'][0].flatten(),sample['edges'][1].flatten()]
+
+
+# reduce size of fake edge attributes so it matches real edges
+edge_attr = edge_attr[:edges[0].size(0)]
+
+# # Run EGNN
+h, x = egnn(h, x, edges, edge_attr)
+
+
+
+
+
+# fig, ax = plt.subplots()
+# ax.imshow(mark_boundaries(sample['img'][0], sample['segments'][0]))
+# for i in range(sample['locations'][0].size(0)):
+#     loc = sample['locations'][0][i]
+#     orientation = sample['orientations'][0][i]
+#     circ = Circle(loc, 0.3, color='red')
+#     slope = np.tan(orientation.numpy())
+#     plot_line(ax, loc, slope)
+#     ax.add_patch(circ)
+# plt.show()
