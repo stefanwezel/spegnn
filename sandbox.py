e2409fb4033dc309b693801d455f71ec0925f3a2
Stefan <stefan.wezel@student.uni-tuebingen.de>
2021-12-21 14:53:23 +0100

    cleanup

--- sandbox.py
+++ sandbox.py
@@ -20,23 +20,40 @@ from PIL import Image
 # from egnn_clean import E_GCL, EGNN, get_edges_batch
 
 
-def collate(batch):
-    batch_feats = [b[0] for b in batch]
-    batch_coords = [b[1] for b in batch]
-    batch_targets = [b[2] for b in batch]
-    batch_feats = torch.nn.utils.rnn.pad_sequence(batch_feats,batch_first=True)
-    batch_coords = torch.nn.utils.rnn.pad_sequence(batch_coords,batch_first=True)
-    n_nodes = batch_feats.shape[1]
-    batch_size = batch_feats.shape[0]
-    edges, edge_attr = get_edges_batch(n_nodes, batch_size)
-    batch_coords = batch_coords.view(-1,2) / 28
-    batch_feats = batch_feats.view(batch_coords.shape[0],-1)
-    batch_targets = torch.LongTensor(batch_targets)
+
+
+def collate_edges(batch):
+    """ TODO """
+    batch_imgs = [b['img_rgb'] for b in batch]
+    batch_locations = [b['locations'] for b in batch]
+    batch_orientations = [b['orientations'] for b in batch]
+    batch_targets = [b['target'] for b in batch]
+    batch_segments = [b['segments'] for b in batch]
+    batch_locations = torch.nn.utils.rnn.pad_sequence(batch_locations,batch_first=True)
+    batch_orientations = torch.nn.utils.rnn.pad_sequence(batch_orientations,batch_first=True)
+    # print(batch_orientations.size())
+    # n_nodes = batch_locations.shape[1]
+    # batch_size = batch_locations.shape[0]
+    # edges, edge_attr = get_edges_batch(n_nodes, batch_size)
+    # batch_orientations = batch_orientations.view(-1,2) / 28
+    # batch_locations = batch_locations.view(batch_orientations.shape[0],-1)
+    # batch_targets = torch.LongTensor(batch_targets)
     
-    return [batch_feats,batch_coords,batch_targets,edges,n_nodes,batch_size]
+    # return [batch_locations,batch_orientations,batch_targets,edges,n_nodes,batch_size]
+    return dict(
+                img=batch_imgs,
+                locations=batch_locations,
+                orientations=batch_orientations,
+                target=batch_targets,
+                segments=batch_segments,
+        )
+
+
+
 
 
 def plot_line(ax, center, slope, length=1):
+    """ TODO """
     if slope > 30:
         slope = 30
     elif slope < -30:
@@ -51,8 +68,6 @@ def plot_line(ax, center, slope, length=1):
     else:
         length=length
 
-
-
     b = center[1] - slope * center[0]
     pt1 = (center[0] - length, slope * (center[0] - length) + b)
     pt2 = (center[0] + length, slope * (center[0] + length) + b)
@@ -64,22 +79,22 @@ def plot_line(ax, center, slope, length=1):
 
 
 def get_neighbors(node, edges):
-    # print(node)
-    # print(edges)
-
-    return list(set([edge[1] for edge in edges if edge[0]==node] + [edge[0] for edge in edges if edge[1]==node]))
+    """ TODO """
+    return list(
+        set(
+            [edge[1] for edge in edges if edge[0]==node] + [edge[0] for edge in edges if edge[1]==node]
+            ))
 
 
 def get_highest_contrast_neighbor(g, node, neighbors):
-    
+    """ TODO """
     c1 = g.nodes[node]['mean color']
     highest_contrast_neighbor = None
     highest_contrast = -np.inf
     for neighbor in neighbors:
-
         c2 = g.nodes[neighbor]['mean color']
         dist = distance.euclidean(c1, c2)
-        if dist >= highest_contrast:
+        if dist > highest_contrast:
             highest_contrast_neighbor = neighbor
             highest_contrast = dist                    
     return highest_contrast_neighbor
@@ -89,123 +104,74 @@ def get_highest_contrast_neighbor(g, node, neighbors):
 
 class SuperpixelDataset(torch.utils.data.Dataset):
     """Invariant dataset."""
-    def __init__(self,orig_dataset, num_edges):
+    def __init__(self,orig_dataset):
         """
         Args:
             orig_dataset (Dataset): dataset
         """
         self.orig_dataset = orig_dataset
-        self.num_edges = num_edges
+
     def __len__(self):
         return len(self.orig_dataset)
 
     def __getitem__(self, idx):
+
         img,target = self.orig_dataset[idx]
-        # x.unsqueeze_(0)
-        # img = data[s,0,:,:].numpy()
-        # print(img.shape)
-
-        # print(img.min())
-
+        # 'convert' to rgb
         img_rgb = np.zeros((28,28,3))
         img_rgb[:,:,2] = img
         img_rgb[:,:,1] = img
         img_rgb[:,:,0] = img
 
-        # print(img_rgb[:,:,0] == img_rgb[:,:,1])
-        # print(img_rgb.shape)
-
 
         img = np.float32(np.asarray(img[0,:,:]))/255
-        # img_rgb = Image.fromarray(img.astype(int), 'RGB')
-        # print(img_rgb.size)
-        # print(img.shape)
-        # img_rgb = torch.from_numpy(img).unsqueeze(0)
-        # img_rgb = img_rgb.repeat(3, 1, 1)
-        # img_rgb = img_rgb.reshape(28,28,3)
-        # print(img_rgb.size())
-
         labels = slic(img_rgb, n_segments = 15, sigma = 0.1, start_label=0)
-
-
-
         p = regionprops(labels+1,intensity_image=img)
         g = graph.rag_mean_color(img, labels)
-        # print(g)
-        # print(g.nodes[0].keys())
-        # print(g.nodes[0]['labels'][0])
-        # print(g.edges)
-        # print(get_neighbors(g.nodes[0]['labels'][0], g.edges))
-        # print(g.neighbors(5))
-        # print(dir(g))
 
         feats = []
         coords = []
      
+        edge_locations = []
+        edge_orientations = []
 
         for node in g.nodes:
             neighbors = get_neighbors(node, g.edges)
             highest_contrast_neighbor = get_highest_contrast_neighbor(g, node, neighbors)
-            print(highest_contrast_neighbor)
-            print()
+            edge_center = (np.array(p[node].centroid) + np.array(p[highest_contrast_neighbor].centroid))/2
 
+            edge_orientation = np.rad2deg(
+                np.arctan2(
+                    p[highest_contrast_neighbor].centroid[-1] - p[highest_contrast_neighbor].centroid[0],
+                    p[node].centroid[-1] - p[node].centroid[0]
+                    ))
             
-            color = p[node]['mean_intensity']
-            # print(dir(g.neighbors(node)))
-            # print(color)
-            orientation = p[node].orientation#['orientation']
-            # print(orientation)
-            invariants = p[node]['moments_hu']
+            edge_locations.append(torch.Tensor(np.array(edge_center)))
+            # edge_locations.append(torch.Tensor(np.array(p[node].centroid)))
+            edge_orientations.append(edge_orientation)
 
-            # print(p[node]['weighted_moments_hu'])
-            # print(dir(p[node]))
+            color = p[node]['mean_intensity']
+            orientation = p[node].orientation#['orientation']
+            invariants = p[node]['moments_hu']
             center = torch.Tensor(p[node]['centroid']).unsqueeze(0)
             feat = torch.cat([torch.Tensor([color]),torch.Tensor(invariants)]).unsqueeze(0)
             feats.append(feat)
             coords.append(center)
 
-
-            # print(dir(p[node]))
-
-        # print(g)
-        # print(p)
-        # print()
-
-        locations = []
-        orientations = []
-
-
-        for edge in list(g.edges)[:self.num_edges]: # 'cropping'
-            node1 = edge[0]
-            node2 = edge[1]
-
-
-            # print(p[node1]['mean_intensity'])
-
-            node1_loc = p[node1].centroid
-            node2_loc = p[node2].centroid
-
-            edge_loc = (np.array(node1_loc) + np.array(node2_loc))/2
-            locations.append(torch.Tensor(np.array([edge_loc])))
-
-            orientation = np.rad2deg(np.arctan2(node2_loc[-1] - node2_loc[0], node1_loc[-1] - node1_loc[0]))
-            orientations.append(torch.Tensor(np.array([orientation])))
-
-        # 'padding'
-        if len(g.edges) <= self.num_edges:
-            for i in range(self.num_edges - len(g.edges)):
-                locations.append(locations[i])
-                orientations.append(orientations[i])
-
-
-        locations = torch.cat(locations , dim=0)
-        orientations = torch.cat(orientations, dim=0)
-
-
+        edge_locations = torch.stack(edge_locations, dim=0)#.unsqueeze(0)
+        edge_orientations = torch.Tensor(np.array(edge_orientations))#.unsqueeze(0)
         feats = torch.cat(feats,dim=0)
         coords = torch.cat(coords,dim=0)
 
-        return dict(img=img, locations=locations, orientations=orientations, target=target, labels=labels, img_rgb=img_rgb)
+
+        # print(coords)
+        return dict(
+            img_rgb=img_rgb,
+            locations=edge_locations,
+            orientations=edge_orientations,
+            target=target,
+            segments=labels,
+            )
 
 
 
@@ -222,59 +188,23 @@ total_transform_test = transforms.Compose([
 
 
 
-mnist_train = SuperpixelDataset(datasets.MNIST('data', train=True, download=True,transform=total_transform), num_edges=70)
-mnist_test = SuperpixelDataset(datasets.MNIST('data', train=False, download=True,transform=total_transform_test), num_edges=70)
+mnist_train = SuperpixelDataset(datasets.MNIST('data', train=True, download=True,transform=total_transform))
+mnist_test = SuperpixelDataset(datasets.MNIST('data', train=False, download=True,transform=total_transform_test))
 
 
-
-
-
-trainloader = torch.utils.data.DataLoader(mnist_train, batch_size=1, shuffle=True)
-
-
+trainloader = torch.utils.data.DataLoader(mnist_train, batch_size=10, shuffle=True, collate_fn=collate_edges)
 sample = next(iter(trainloader))
 
 
+print(sample.keys())
 
-
-
-
-
-# image = sample['img_rgb'][0]
-# backtorgb = cv2.cvtColor(image,cv2.COLOR_GRAY2RGB)
-# img_rgb = np.zeros((img.shape[0], img.shape[1], 3))
-# for d in range(3):
-#     img_rgb[:,:,d] = img
-# print(img_rgb.shape)
-# fig, ax = plt.subplots(2)
-# image = img_as_float(io.imread('/home/stefan/Downloads/mnist_2_small.png'))[:,:,:3]
-# image = cv2.imread('image.png')
-# segments = slic(image, n_segments = 15, sigma = 0.1, start_label=0)
-# plt.imshow(sample['img'][0])
-# print(segments==sample['labels'][0].numpy())
-# print()
-
-
-
-# plt.imshow(mark_boundaries(sample['img_rgb'][0], sample['labels'][0].numpy()))
-# plt.show()
-
-# plt.imshow(mark_boundaries(sample['img'][0], sample['labels'][0].numpy()), alpha=0.3)
-# ax[0].imshow(sample['img'][0], cmap='Blues_r')
-
-# for i in range(sample['locations'][0].size(0)):
-#     loc = sample['locations'][0][i]
-#     orientation = sample['orientations'][0][i]
-#     circ = Circle(loc, 0.3, color='red')
-#     slope = np.tan(orientation.numpy())
-
-#     plot_line(ax[0], loc, slope)
-#     ax[0].add_patch(circ)
-
-
-# ax[0].imshow(mark_boundaries(sample['img'][0], sample['labels'][0].numpy()))
-
-
-# # segments = slic(sample['img'][0], n_segments=25, compactness=0.5, sigma=0.1, start_label=1)
-# # ax[0].imshow(mark_boundaries(sample['img'][0], segments))
-# plt.show()
+fig, ax = plt.subplots()
+ax.imshow(mark_boundaries(sample['img'][0], sample['segments'][0]))
+for i in range(sample['locations'][0].size(0)):
+    loc = sample['locations'][0][i]
+    orientation = sample['orientations'][0][i]
+    circ = Circle(loc, 0.3, color='red')
+    slope = np.tan(orientation.numpy())
+    plot_line(ax, loc, slope)
+    ax.add_patch(circ)
+plt.show()
